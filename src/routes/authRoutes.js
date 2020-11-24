669bd608b16f510ad65b5f6e84e94d029ae0eafa
chelskeeley <chelsea.keeley@gmail.com>
2020-11-24 14:50:59 -0500

    Throw mongo error

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,9 +4,17 @@ const User = mongoose.model("User");
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-  console.log(req.body);
-  res.send("You made a post request!");
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = new User({ email, password });
+    await user.save();
+
+    res.send("You made a post request!");
+  } catch (e) {
+    return res.status(422).send(e.message);
+  }
 });
 
 module.exports = router;
